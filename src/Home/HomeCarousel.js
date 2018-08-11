c7e0f36e33ee180f67828d6e9d61e1cdcfd14799
sd <sdebaun74@gmail.com>
2018-08-11 14:36:48 -0700

    screw you react-slick

--- src/Home/HomeCarousel.js
+++ src/Home/HomeCarousel.js
@@ -4,25 +4,27 @@ import {
   Grid,
   Header,
   Icon,
+  Button,
 } from 'semantic-ui-react';
 import Slider from 'react-slick';
 import { Query } from 'react-apollo';
 import { QUERY_ACTIVE_PROJECTS_DETAILED } from '../api/projects';
 
 const HomeSlide = ({project}) =>
-  <div style={{backgroundColor: 'white'}}>
+  <div style={{backgroundColor: 'white'}} style={{margin: '0px 20px'}}>
   <Header as='h1'>{project.title}</Header>
   </div>
 
 const SETTINGS = {
-  adaptiveHeight: true,
-  autoplay: true,
-  centerMode: true,
-  infinite: true,
-  dots: true,
-}
+    adaptiveHeight: true,
+    autoplay: true,
+    // centerMode: true,
+    infinite: true,
+    dots: true,
+  }
+  
 const HomeCarousel = () =>
-  <div style={{padding: '2rem'}}>
+  <div style={{padding: '40px'}}>
   <Query query={QUERY_ACTIVE_PROJECTS_DETAILED}>
     {({ loading, data: { projectsActive } }) => {
       if (loading) { return <div>LOADING...</div> }
@@ -31,6 +33,5 @@ const HomeCarousel = () =>
       </Slider>
     }}
   </Query>
-  </div>
-
+</div>
 export default HomeCarousel
